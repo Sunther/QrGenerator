c506d86a763e5221da6d483731ab6b995aaebf90
Sunther <wii250.kart@gmail.com>
2024-09-30 08:47:23 +0200

    Added disk management classes

---
+++ QrGenerator.Disk/Implementations/ExplorerManagement.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace QrGenerator.Disk;
+
+public static class ExplorerManagement
+{
+    public static void OpenFolderContainingFile(string filePath)
+    {
+        var folderPath = Path.GetDirectoryName(filePath);
+
+        if (!Directory.Exists(folderPath))
+        {
+            throw new DirectoryNotFoundException($"The directory '{folderPath}' does not exist.");
+        }
+
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = "explorer.exe",
+            Arguments = folderPath,
+            UseShellExecute = true,
+            CreateNoWindow = true,
+        });
+    }
+}
