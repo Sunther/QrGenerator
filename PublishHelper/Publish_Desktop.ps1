6d7f7ff555ba5cdd205f94b521040a29d2b2bfff
Sunther <wii250.kart@gmail.com>
2024-10-17 18:22:12 +0200

    Adding new version

--- PublishHelper/Publish_Desktop.ps1
+++ PublishHelper/Publish_Desktop.ps1
@@ -1,9 +1,9 @@
+Set-Location ".."
+
 dotnet publish "QrGenerator.Desktop" `
     -f "net8.0-windows10.0.26100.0" `
     -c "Release" `
     -p:RuntimeIdentifierOverride=win10-x64 `
     -p:WindowsPackageType=None `
     --verbosity quiet `
-    -o "C:\TEMP\Publish\QrGenerator"
-
-explorer "C:\TEMP\Publish"
+    -o ".\PublishHelper\QrGenerator"
